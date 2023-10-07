58c82e89dc97e5e0eccfd0b513d40f1f129f9bd4
Phuoc Nguyen <phuoc94@hotmail.fi>
2023-10-07 22:42:43 +0300

    update footer

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -2,26 +2,21 @@ import React, { Fragment } from 'react';
 
 import { Outlet } from 'react-router-dom';
 
-import { Box, Container, Typography } from '@mui/material';
+import { Box } from '@mui/material';
 
 import NavBar from '../containers/navbar/NavBar';
+import Footer from './Footer';
 
 const Layout: React.FC = () => {
   return (
     <Fragment>
       <NavBar />
 
-      <Box component="main" sx={{ backgroundColor: '#dbdcdc' }}>
+      <Box component="main">
         <Outlet />
       </Box>
 
-      <Box component="footer">
-        <Container>
-          <Typography variant="h2" component="h1">
-            This is the footer
-          </Typography>
-        </Container>
-      </Box>
+      <Footer />
     </Fragment>
   );
 };
