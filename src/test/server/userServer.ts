06ac955fe82ca06a9bb0ab4c94114cbe2bc30d14
Phuoc Nguyen <phuoc94@hotmail.fi>
2023-10-07 11:47:59 +0300

    add test for auth

--- src/test/server/userServer.ts
+++ src/test/server/userServer.ts
@@ -4,6 +4,7 @@ import { setupServer } from 'msw/node';
 import usersData from '../data/usersData';
 
 export const access_token = 'my-access-token';
+export const refresh_token = 'my-refresh-token';
 
 export const handlers = [
   rest.post(
@@ -15,11 +16,52 @@ export const handlers = [
       );
       if (foundUser) {
         const token = access_token + '_' + foundUser.id;
-        console.log('token', token);
         return res(ctx.json({ access_token: token }));
       } else {
-        ctx.status(401);
-        return res(ctx.text('Cannot authenticate user'));
+        return res(
+          ctx.status(401),
+          ctx.json({
+            errorMessage: `Unauthorized`,
+          }),
+        );
+      }
+    },
+  ),
+  rest.post(
+    'https://api.escuelajs.co/api/v1/auth/refresh-token',
+    async (req, res, ctx) => {
+      const { refreshToken } = await req.json();
+      const userid = refreshToken.split('_')[1];
+      const foundUser = usersData.find((u) => u.id === Number(userid));
+      if (foundUser) {
+        const token = access_token + '_' + foundUser.id;
+        return res(ctx.json({ access_token: token }));
+      } else {
+        return res(
+          ctx.status(401),
+          ctx.json({
+            errorMessage: `Unauthorized`,
+          }),
+        );
+      }
+    },
+  ),
+  rest.get(
+    'https://api.escuelajs.co/api/v1/auth/profile',
+    async (req, res, ctx) => {
+      const authorization = req.headers.get('Authorization');
+      const token = authorization?.split(' ')[1];
+      const userid = token?.split('_')[1];
+      const foundUser = usersData.find((u) => u.id === Number(userid));
+      if (foundUser) {
+        return res(ctx.json(foundUser));
+      } else {
+        return res(
+          ctx.status(401),
+          ctx.json({
+            errorMessage: `Unauthorized`,
+          }),
+        );
       }
     },
   ),
--- src/test/store/authSlice.test.ts
+++ src/test/store/authSlice.test.ts
@@ -1,6 +1,11 @@
-import { login } from '../../store/actions/auth.actions';
+import {
+  fetchNewAccessToken,
+  getProfile,
+  login,
+} from '../../store/actions/auth.actions';
 import { createStore } from '../../store/configureStore';
-import server, { access_token } from '../server/userServer';
+import usersData from '../data/usersData';
+import server, { access_token, refresh_token } from '../server/userServer';
 
 let store = createStore();
 
@@ -19,6 +24,45 @@ describe('Test authReducer async actions', () => {
     await store.dispatch(
       login({ email: 'john@mail.com', password: 'changeme' }),
     );
-    expect(store.getState().auth.accessToken).toBe(access_token + '_1');
+    expect(store.getState().auth.accessToken).toBe(
+      `${access_token}_${usersData[0].id}`,
+    );
+  });
+
+  test("Shouldn't login user if wrong credentials", async () => {
+    await store.dispatch(
+      login({ email: 'john@mail.com', password: 'changemeaasd' }),
+    );
+    expect(store.getState().auth.error).toBe(
+      'Request failed with status code 401',
+    );
+  });
+
+  test('Should get a new access token with a refresh token', async () => {
+    await store.dispatch(
+      fetchNewAccessToken(`${refresh_token}_${usersData[0].id}`),
+    );
+    expect(store.getState().auth.accessToken).toBe(
+      `${access_token}_${usersData[0].id}`,
+    );
+  });
+
+  test("Shouldn't get a new access token with a wrong refresh token", async () => {
+    await store.dispatch(fetchNewAccessToken(`${refresh_token}_wrong`));
+    expect(store.getState().auth.error).toBe(
+      'Request failed with status code 401',
+    );
+  });
+
+  test('Should get the profile the current user with session', async () => {
+    await store.dispatch(getProfile(`${access_token}_${usersData[1].id}`));
+    expect(store.getState().auth.profile).toMatchObject(usersData[1]);
+  });
+
+  test("Shouldn't get the profile the current user with a wrong session", async () => {
+    await store.dispatch(getProfile(`${access_token}_wrong`));
+    expect(store.getState().auth.error).toBe(
+      'Request failed with status code 401',
+    );
   });
 });
